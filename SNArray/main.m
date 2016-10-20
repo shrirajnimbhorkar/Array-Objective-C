85f4b0dacb3794ede89d4a5de207538aff4e72f8
Shriraj Nimbhorkar <shrirajnimbhorkar@gmail.com>
2016-10-20 12:21:41 +0530

    Inital Commit

---
+++ SNArray/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  SNArray
+//
+//  Created by Student P_05 on 19/10/16.
+//  Copyright © 2016 Shriraj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    
+    
+    NSMutableArray *mArray = [[NSMutableArray alloc]init];
+
+    [mArray addObject:@"Shriraj"];
+    [mArray addObject:@"Hello World"];
+    [mArray addObject:@"Hi"];
+    [mArray addObject:@"Hello Earth"];
+    [mArray addObject:@"Hacker rank"];
+        
+        NSLog(@"%@",mArray);
+        
+    
+    
+    }
+    return 0;
+}
+
+    
+    
+    
+    
+
